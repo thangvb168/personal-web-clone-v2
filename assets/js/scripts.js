8fdba5689beef5adda6669aadcad04b577843154
vbthang <2151062875@e.tlu.edu.vn>
2025-03-23 19:29:02 +0700

    feat: add modal component css

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // Open Collapse
     const collpaseBtn = document.getElementById("collapse-btn");
     const navbarCollapse = document.getElementById("navbar-collapse");
-    
+
     collpaseBtn.addEventListener("click", function () {
         navbarCollapse.classList.toggle("show");
     });
@@ -17,6 +17,44 @@ document.addEventListener("DOMContentLoaded", function () {
                 if (dropdownMenu && dropdownMenu.classList.contains("dropdown-menu")) {
                     dropdownMenu.classList.toggle("show");
                 }
+            }
+        });
+    });
+
+    const btnOpenModal1 = document.getElementById('btn-open-modal-1');
+    const btnOpenModal2 = document.getElementById('btn-open-modal-2');
+
+    const modal1 = document.getElementById('modal-1');
+    const modal2 = document.getElementById('modal-2');
+
+    const btnClose = document.querySelectorAll('.btn-close');
+
+    function openModal(modal) {
+        modal.classList.add('show');
+        document.body.classList.add('modal-open');
+    }
+
+    function closeModal(modal) {
+        modal.classList.remove('show');
+        document.body.classList.remove('modal-open');
+    }
+
+    btnOpenModal1.addEventListener('click', () => openModal(modal1));
+    btnOpenModal2.addEventListener('click', () => openModal(modal2));
+
+    btnClose.forEach(btn => {
+        btn.addEventListener('click', function () {
+            console.log('close button clicked');
+            const modal = this.closest('.modal');
+            closeModal(modal);
+        });
+    });
+
+    document.querySelectorAll('.modal').forEach(modal => {
+        modal.addEventListener('click', function (event) {
+            console.log('modal clicked');
+            if (!event.target.closest('.modal-content')) {
+                closeModal(modal);
             }
         });
     });
